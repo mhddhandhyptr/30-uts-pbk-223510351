022c58fab96820a3a6d3a1110e7d5829004ff61a
ferdialdinata <ferdiardinata367@gamil.com>
2024-05-14 19:19:18 +0700

    penambahan posts

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,74 @@
 <template>
-  <div>
-    <h1>Catatan Tugas</h1>
-    <form>
-      <label for="activity">Tugas:</label>
-      <input type="text" id="activity" v-model="newActivity"><br><br>
-      <button type="button" @click="addActivity">Tambah</button>
-      <button type="button" @click="filterUnfinished">Filter Halaman</button>
-    </form>
-    <ul>
-      <li v-for="(activity, index) in activities" :key="index" :style="{textDecoration: activity.completed ? 'line-through' : 'none'}">
-        {{ activity.name }}
-        <button @click="removeActivity(index)">Hapus</button>
-        <button @click="completeActivity(index)" v-if="!activity.completed">Selesai</button>
-      </li>
-    </ul>
+  <div id="app">
+    <!-- Header -->
+    <header>
+      <nav>
+        <ul>
+          <li @click="showTodos">Todos</li>
+          <li @click="showPosts">Posts</li>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- Main Content -->
+    <main>
+      <div v-if="activeMenu === 'todos'">
+        <!-- Fitur Todos -->
+        <h2>Todos</h2>
+        <form>
+          <label for="activity">Tugas:</label>
+          <input type="text" id="activity" v-model="newActivity"><br><br>
+          <button type="button" @click="addActivity">Tambah</button>
+          <button type="button" @click="filterUnfinished">Filter Halaman</button>
+        </form>
+        <ul>
+          <li v-for="(activity, index) in activities" :key="index" :style="{textDecoration: activity.completed ? 'line-through' : 'none'}">
+            {{ activity.name }}
+            <button @click="removeActivity(index)">Hapus</button>
+            <button @click="completeActivity(index)" v-if="!activity.completed">Selesai</button>
+          </li>
+        </ul>
+      </div>
+      <div v-else-if="activeMenu === 'posts'">
+        <!-- Fitur Postingan -->
+        <h2>Postingan</h2>
+        <select v-model="selectedUser">
+          <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
+        </select>
+        <div v-if="selectedUser">
+          <div v-for="post in filteredPosts" :key="post.id">
+            <h3>{{ post.title }}</h3>
+            <p>{{ post.body }}</p>
+          </div>
+        </div>
+        <div v-else>
+          <p>Silakan pilih pengguna untuk melihat postingan mereka.</p>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'App',
   data() {
     return {
+      activeMenu: 'todos',
       activities: [],
       newActivity: "",
+      users: [],
+      posts: [],
+      selectedUser: null
     };
   },
   methods: {
+    showTodos() {
+      this.activeMenu = 'todos';
+    },
+    showPosts() {
+      this.activeMenu = 'posts';
+    },
     addActivity() {
       if (this.newActivity !== "") {
         this.activities.push({ name: this.newActivity, completed: false });
@@ -44,19 +87,39 @@ export default {
       });
     },
   },
+  mounted() {
+    // Ambil data user dari API
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        this.users = data;
+      });
+
+    // Ambil data postingan dari API
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        this.posts = data;
+      });
+  },
+  computed: {
+    filteredPosts() {
+      // Filter postingan berdasarkan user yang dipilih
+      return this.posts.filter(post => post.userId === parseInt(this.selectedUser));
+    }
+  }
 };
 </script>
 
-<style>
+<style scoped>
 body {
   background: url('/src/bground.jpeg') no-repeat center center fixed;
   background-size: cover;
-
   margin: 0;
   padding: 0;
 }
 
-h1 {
+h1, h2 {
   color: black;
   margin-top: 50px;
   text-align: center;
@@ -140,6 +203,4 @@ li button:last-child:hover {
 li.completed {
   text-decoration: line-through;
 }
-
-
 </style>
